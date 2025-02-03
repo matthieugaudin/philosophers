671a08f26f067d84506e49504f299779a3f76fb2
Matthieu Gaudin <mgaudin@2A4.42angouleme.fr>
2025-02-03 12:26:14 +0100

    chore: set up project folder structure

---
+++ philo/include/philosophers.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgaudin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 08:48:59 by mgaudin           #+#    #+#             */
+/*   Updated: 2025/02/03 09:01:46 by mgaudin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <semaphore.h>
+
+#endif /* PHILOSOPHERS_H */
