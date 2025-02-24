4dc08f76c19cbfbd561065567096568393f89671
Matthieu Gaudin <mgaudin@2E1.42angouleme.fr>
2025-02-24 11:01:41 +0100

    refactor(free): destroy mutexes without recreating the function in free.c

--- philo/src/free.c
+++ philo/src/free.c
@@ -6,7 +6,7 @@
 /*   By: mgaudin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:38:30 by mgaudin           #+#    #+#             */
-/*   Updated: 2025/02/18 15:23:51 by mgaudin          ###   ########.fr       */
+/*   Updated: 2025/02/24 10:57:21 by mgaudin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,13 @@ void	free_mutexes(t_env *env, int i, int status)
 		pthread_mutex_destroy(&env->print_mtx);
 	if (status >= 2)
 		pthread_mutex_destroy(&env->is_over_mtx);
-	if (status >= 3)
+	if (status == 3)
 		pthread_mutex_destroy(&env->meals_eaten_mtx);
 }
 
 void	free_data(t_env *env)
 {
-	int	i;
-
-	i = 0;
-	while (i < env->nb_philo)
-	{
-		pthread_mutex_destroy(&env->forks[i]);
-		i++;
-	}
-	pthread_mutex_destroy(&env->print_mtx);
-	pthread_mutex_destroy(&env->is_over_mtx);
-	pthread_mutex_destroy(&env->last_meal_mtx);
-	pthread_mutex_destroy(&env->meals_eaten_mtx);
+	free_mutexes(env, env->nb_philo, 3);
 	free(env->forks);
 	if (env->philos)
 		free(env->philos);
